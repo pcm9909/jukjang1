4b4d26331ea8c0123158619751373a600ea272b9
pcm9909 <57002094+pcm9909@users.noreply.github.com>
2023-06-10 17:39:02 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://ridibooks.com/category/free-books/2200'
+url = 'https://ridibooks.com/bestsellers/comic'
 response = requests.get(url)
 response.encoding = 'utf-8'
 html = response.text
 
 soup = BeautifulSoup(html, 'html.parser')
 
-bookservices = soup.select('fig-1rl9mz1')
+bookservices = soup.select('.fig-yo8n1d')
 for no, book in enumerate(bookservices, 1):
     print(no, book.text.strip())
