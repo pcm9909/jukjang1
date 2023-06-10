9ea5d15d1301920d0b9c8f8c121ae171e856966d
pcm9909 <57002094+pcm9909@users.noreply.github.com>
2023-06-10 18:14:09 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://ridibooks.com/category/new-releases/2200'
+url = 'https://product.kyobobook.co.kr/bestseller/online?period=002#?page=1&per=20&ymw=&period=002&saleCmdtClstCode=&dsplDvsnCode=000&dsplTrgtDvsnCode=001&saleCmdtDsplDvsnCode='
 response = requests.get(url)
 response.encoding = 'utf-8'
 html = response.text
 
 soup = BeautifulSoup(html, 'html.parser')
 
-bookservices = soup.select('.title_text')
+bookservices = soup.select('.prod_name')
 for no, book in enumerate(bookservices, 1):
     print(no, book.text.strip())
