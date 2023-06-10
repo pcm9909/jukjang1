60da9a0e268226411988c65281e93f75059ae149
pcm9909 <57002094+pcm9909@users.noreply.github.com>
2023-06-10 18:04:13 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+"""
 import requests
 from bs4 import BeautifulSoup
 
@@ -11,3 +12,18 @@ soup = BeautifulSoup(html, 'html.parser')
 bookservices = soup.select('.prod_name')
 for no, book in enumerate(bookservices, 1):
     print(no, book.text.strip())
+    """
+
+import requests
+
+## HTTP GET Request
+req = requests.get('https://beomi.github.io/beomi.github.io_old/')
+
+## HTML 소스 가져오기
+html = req.text
+## HTTP Header 가져오기
+header = req.headers
+## HTTP Status 가져오기 (200: 정상)
+status = req.status_code
+## HTTP가 정상적으로 되었는지 (True/False)
+is_ok = req.ok
