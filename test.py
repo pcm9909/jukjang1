4edaacb91ede38ab9dc607ccfd1443b77d64ed72
pcm9909 <57002094+pcm9909@users.noreply.github.com>
2023-06-10 18:08:10 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,29 +1,13 @@
-"""
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://product.kyobobook.co.kr/bestseller/online?period=001'
+url = 'https://ridibooks.com/category/new-releases/2200'
 response = requests.get(url)
 response.encoding = 'utf-8'
 html = response.text
 
 soup = BeautifulSoup(html, 'html.parser')
 
-bookservices = soup.select('.prod_name')
+bookservices = soup.select('.title_text')
 for no, book in enumerate(bookservices, 1):
     print(no, book.text.strip())
-    """
-
-import requests
-
-## HTTP GET Request
-req = requests.get('https://beomi.github.io/beomi.github.io_old/')
-
-## HTML 소스 가져오기
-html = req.text
-## HTTP Header 가져오기
-header = req.headers
-## HTTP Status 가져오기 (200: 정상)
-status = req.status_code
-## HTTP가 정상적으로 되었는지 (True/False)
-is_ok = req.ok
