4e48c2cc893e348a6950c7fc310dd1a2c76bc1e3
pcm9909 <57002094+pcm9909@users.noreply.github.com>
2023-06-10 18:40:31 +0900

    Update test.py

--- test.py
+++ test.py
@@ -2,12 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 
 url = 'https://ridibooks.com/category/new-releases/2200'
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
+response = requests.get(url)
+response.encoding = 'utf-8'
+html = response.text
 
-response = requests.get(url, headers=headers)
-response.raise_for_status()  # 오류가 발생하면 예외를 발생시킴
-
-soup = BeautifulSoup(response.text, 'html.parser')
+soup = BeautifulSoup(html, 'html.parser')
 
 bookservices = soup.select('.title_text')
 for no, book in enumerate(bookservices, 1):
