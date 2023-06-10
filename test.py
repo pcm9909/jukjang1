e67ace3e1bec79d3487a91c59faf34430a4ee3d0
pcm9909 <57002094+pcm9909@users.noreply.github.com>
2023-06-10 17:34:49 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://ridibooks.com/category/new-releases/2200'
+url = 'https://ridibooks.com/category/free-books/2200'
 response = requests.get(url)
 response.encoding = 'utf-8'
 html = response.text
 
 soup = BeautifulSoup(html, 'html.parser')
 
-bookservices = soup.select('.title_text')
+bookservices = soup.select('fig-1rl9mz1')
 for no, book in enumerate(bookservices, 1):
     print(no, book.text.strip())
