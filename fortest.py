7a9446604e3a3fb860690353328d09e368e94c23
pcm9909 <57002094+pcm9909@users.noreply.github.com>
2023-06-10 18:02:18 +0900

    Create fortest.py

---
+++ fortest.py
@@ -0,0 +1,13 @@
+import requests
+
+## HTTP GET Request
+req = requests.get('https://beomi.github.io/beomi.github.io_old/')
+
+## HTML 소스 가져오기
+html = req.text
+## HTTP Header 가져오기
+header = req.headers
+## HTTP Status 가져오기 (200: 정상)
+status = req.status_code
+## HTTP가 정상적으로 되었는지 (True/False)
+is_ok = req.ok
